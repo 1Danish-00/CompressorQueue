94c9fdb8efaac8b08789e2e764292a0b521db6ee
Uchiha34 <90205302+Uchiha34@users.noreply.github.com>
2021-09-08 18:58:49 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("CHAT GROUP", url="t.me/fateunionchat"),
+                Button.url("CHANNEL", url="t.me/for_otaku"),
             ],
         ],
     )
@@ -59,8 +59,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("CHAT GROUP", url="t.me/fateunionchat"),
+                Button.url("CHANNEL", url="t.me/for_otaku"),
             ],
         ],
     )
