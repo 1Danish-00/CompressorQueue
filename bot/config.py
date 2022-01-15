ddede55d074db84e5dcf3e0ee5c32a82545991f2
arrshalt <97388525+arrshalt@users.noreply.github.com>
2022-01-15 09:43:07 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -25,7 +25,7 @@ try:
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
-    THUMB = config(
+        THUMB = config(
         "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
     )
 except Exception as e:
