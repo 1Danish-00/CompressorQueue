464a0e8b946e6b38b88f216b265603856d827e3e
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-02-20 12:13:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -89,19 +89,19 @@ async def _(e):
 
 @bot.on(events.NewMessage(pattern="/usage"))
 async def _(e):
-    if str(event.sender_id) not in OWNER:
-        if event.sender_id != DEV:
+    if str(e.sender_id) not in OWNER:
+        if e.sender_id != DEV:
             return
     total, used, free = shutil.disk_usage(".")
     cpuUsage = psutil.cpu_percent()
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    upload = humanbytes(psutil.net_io_counters().bytes_sent)
-    down = humanbytes(psutil.net_io_counters().bytes_recv)
-    TOTAL = humanbytes(total)
-    USED = humanbytes(used)
-    FREE = humanbytes(free)
-    await e.reply("**TOTAL DISK SPACE**: `{}`\n**USED**: `{}`\n**FREE**: {}\n**UPlOAD**: `{}`\n**CPU**: `{}`\n**RAM**: `{}`\n**DISK**: `{}`".format(
+    upload = hbs(psutil.net_io_counters().bytes_sent)
+    down = hbs(psutil.net_io_counters().bytes_recv)
+    TOTAL = hbs(total)
+    USED = hbs(used)
+    FREE = hbs(free)
+    await e.reply("**TOTAL DISK SPACE**: `{}`\n**USED**: `{}`\n**FREE**: {}\n**UPlOAD**: `{}`\n**DOWNLOAD**: `{}`\n**CPU**: `{}%`\n**RAM**: `{}%`\n**DISK**: `{}%`".format(
         TOTAL,
         USED,
         FREE,
