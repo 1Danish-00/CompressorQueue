ce10016b0feb1a6ba1e4254f837f6026677aaa23
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-10-15 15:50:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -26,7 +26,7 @@ try:
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     THUMB = config(
-        "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
+        "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
     )
 except Exception as e:
     print("Environment vars Missing")
